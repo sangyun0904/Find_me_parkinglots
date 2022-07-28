be7011f40a865a772654136f3cef2f384f626c43
sangyoon kim <sangyun0904@gmail.com>
2022-07-28 20:02:17 +0900

    Add bootstrap theme for the main view

--- find_me_parkinglots/urls.py
+++ find_me_parkinglots/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-app_name = 'view_parkinglots'
-
 urlpatterns = [
-    path('view_parkinglots/', include('view_parkinglots.urls')),
     path('admin/', admin.site.urls),
+    path('view_parkinglots/', include('view_parkinglots.urls')),
 ]
--- view_parkinglots/urls.py
+++ view_parkinglots/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
 from . import views
-
+app_name = 'view_parkinglots'
 urlpatterns = [
     path('', views.index, name='index'),
 ]
