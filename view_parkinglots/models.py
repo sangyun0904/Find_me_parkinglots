a9d553d7fcf1bfd520c05d8471b6affb944ddc51
sangyoon kim <sangyun0904@gmail.com>
2022-08-08 21:08:52 +0900

    Add parkinglots data into sqlite with broken p_name

--- view_parkinglots/models.py
+++ view_parkinglots/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Parkinglot (models.Model):
+    
+    p_id = models.CharField(("p_id"), max_length=50)
+    p_name = models.CharField(("p_name"), max_length=200)
+    p_long = models.FloatField(("p_long"))
+    p_lat = models.FloatField(("p_lat"))
+    p_num = models.IntegerField(("p_num"))
--- view_parkinglots/views.py
+++ view_parkinglots/views.py
@@ -1,5 +1,11 @@
+import csv 
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    response = HttpResponse(
+        content_type="text/csv" , 
+        headers={'Content-Disposition' : 'attachment; filename="'}
+    )
+
     return render(request, "index.html")
